1f2f211adb1c4a7828c5d14c46aeb517e4a092f0
pofin <43422520+pofin@users.noreply.github.com>
2020-03-23 11:35:06 -0400

    added inclass work 3-23

---
+++ In-Class-Work/Monday-3-23/3-23.R
@@ -0,0 +1,61 @@
+data("USArrests")
+
+states=row.names(USArrests)
+states
+
+apply(USArrests, 2,mean)
+
+apply(USArrests, 2, var)
+
+pr.out=prcomp(USArrests, scale=TRUE)
+names(pr.out)
+
+pr.out$center
+pr.out$scale
+pr.out$rotation
+
+dim(pr.out$x)
+biplot(pr.out, scale=0)
+pr.out$sdev
+
+pr.var = pr.out$sdev^2
+pr.var
+
+pve = pr.var/sum(pr.var)
+pve
+
+#iris dataset
+data("iris")
+head(iris)
+
+irisdata1 <- iris[,1:4]
+irisdata1
+
+head(irisdata1)
+
+help("princomp")
+principal_components <- princomp(irisdata1, cor = TRUE, score = TRUE)
+summary(principal_components)
+
+plot(principal_components)
+
+plot(principal_components, type = "l")
+
+biplot(principal_components)
+
+#PCA on Boston dataset
+data(Boston, package="MASS")
+help(Boston)
+help(prcomp)
+
+pca_out <- prcomp(Boston, scale. = T)
+pca_out
+plot(pca_out)
+
+help(biplot)
+biplot(pca_out, scale =0)
+boston_pc <- pca_out$x
+boston_pc
+
+head(boston_pc)
+summary(boston_pc)
